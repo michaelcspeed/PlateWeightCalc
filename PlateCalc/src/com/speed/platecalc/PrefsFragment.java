cdbe1c2721576a89a226c24216b6f55bd95a26d7
michaelcspeed <michaelcspeed@gmail.com>
2014-05-06 20:48:45 +0100

    Added summaries to Preferences screen

--- PlateCalc/src/com/speed/platecalc/PrefsFragment.java
+++ PlateCalc/src/com/speed/platecalc/PrefsFragment.java
@@ -1,9 +1,11 @@
 package com.speed.platecalc;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
-public class PrefsFragment extends PreferenceFragment {
+public class PrefsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -12,17 +14,38 @@ public class PrefsFragment extends PreferenceFragment {
 
 		// Load the preferences from an XML resource
 		addPreferencesFromResource(R.xml.preferences);
+		register();
+	}
 
+	private void register() {
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
+		register();
 		setDefaultWeightHint();
 	}
+	
 
 	private void setDefaultWeightHint() {
-		// TODO Auto-generated method stub
+		findPreference("custom_set").setSummary(getPreferenceScreen().getSharedPreferences().getString("custom_set", "None")); 
+		findPreference("default_bar_weight").setSummary(getPreferenceScreen().getSharedPreferences().getString("default_bar_weight", "20")); 
+		
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		
+		if(getPreferenceScreen().getSharedPreferences().getString("default_bar_weight", "").equals("")){
+			SharedPreferences.Editor e = getPreferenceScreen().getSharedPreferences().edit();
+			e.putString("default_bar_weight", "0");
+			e.commit();
+		}
+		
+		setDefaultWeightHint();
 		
 	}
 
